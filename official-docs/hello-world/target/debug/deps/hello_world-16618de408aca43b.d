7fe8af2089368ee3aa5b8916376857f8f5919184
SengHyun <leesnhyun@gmail.com>
2018-04-01 16:01:54 +0900

    [Rust] initial commit

---
+++ official-docs/hello-world/target/debug/deps/hello_world-16618de408aca43b.d
@@ -0,0 +1,5 @@
+/Users/SH/workspace/inswave-rust/official-docs/hello-world/target/debug/deps/hello_world-16618de408aca43b: src/main.rs
+
+/Users/SH/workspace/inswave-rust/official-docs/hello-world/target/debug/deps/hello_world-16618de408aca43b.d: src/main.rs
+
+src/main.rs:
---
+++ official-docs/hello-world/target/debug/hello-world.d
@@ -0,0 +1,1 @@
+/Users/SH/workspace/inswave-rust/official-docs/hello-world/target/debug/hello-world.dSYM: /Users/SH/workspace/inswave-rust/official-docs/hello-world/src/main.rs

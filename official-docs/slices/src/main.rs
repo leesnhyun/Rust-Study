626ac5bd6c4a6f63d984908cd2c799e54b49fabf
SengHyun <leesnhyun@gmail.com>
2018-04-01 17:34:44 +0900

    [Rust] ownership & refrence(with mut)

---
+++ official-docs/slices/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    
+    let s = String::from("hello world");
+
+    let x = first_word(&s);
+
+    //s.clear();
+
+    println!( "x : {}", x );
+
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for(i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
